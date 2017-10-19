2d918c5b3388fe96c4e89905d1b96e248e7e2c34
Ainor Syahrizal <macintorch@gmail.com>
2017-10-19 16:01:56 +0800

    LoginForm setup

--- src/App.js
+++ src/App.js
@@ -7,28 +7,31 @@ import {
 	AUTH_DOMAIN,
 	DATABASE_URL,
 	FIREBASE_PROJECT_ID,
+	FIREBASE_STORAGE_BUCKET,
 	MESSAGE_ID
 } from "react-native-dotenv";
 import firebase from "firebase";
 import reducers from "./reducers";
+import LoginForm from "./components/LoginForm";
 
 class App extends Component {
 	componentWillMount() {
-		firebase.initializeApp({
+		const config = {
 			apiKey: FIREBASE_API_KEY,
 			authDomain: AUTH_DOMAIN,
 			databaseURL: DATABASE_URL,
 			projectId: FIREBASE_PROJECT_ID,
-			storageBucket: "",
+			storageBucket: FIREBASE_STORAGE_BUCKET,
 			messagingSenderId: MESSAGE_ID
-		});
+		};
+
+		firebase.initializeApp(config);
 	}
+
 	render() {
 		return (
 			<Provider store={createStore(reducers)}>
-				<View>
-					<Text>Hello!</Text>
-				</View>
+				<LoginForm />
 			</Provider>
 		);
 	}
