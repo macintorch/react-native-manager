251128f424dda32174a21439ad127c52506dd0c6
Ainor Syahrizal <macintorch@gmail.com>
2017-10-22 14:19:44 +0800

    refactor actions index, employee update reducer setup

--- src/reducers/EmployeeFormReducer.js
+++ src/reducers/EmployeeFormReducer.js
@@ -1,10 +1,19 @@
 import{ EMPLOYEE_UPDATE } from '../actions/types';
 
-const INITIAL_STATE = {};
+const INITIAL_STATE = {
+	name: '',
+	phone: '',
+	shift: ''
+};
 
 export default ( state= INITIAL_STATE, action )=> {
 	swich (action.type) {
+
+		case EMPLOYEE_UPDATE:
+		// keyinterporlation
+		return {...state, [action.payload.prop]: action.payload.value }
+
 		default:
 		return state;
 	}
-};
+}; 
