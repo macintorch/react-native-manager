9dc157416800c292b621293464cae91d20d92eef
Ainor Syahrizal <macintorch@gmail.com>
2017-10-22 13:10:22 +0800

    add 'Add' button in onRight navbar

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,29 @@
 import React from "React";
 import { Scene, Router, Stack } from "react-native-router-flux";
 import LoginForm from "./components/LoginForm";
+import EmployeeList from "./components/EmployeeList";
 
 const RouterComponent = () => {
 	return (
 		<Router>
-			<Stack key="root">
-				<Scene key="login" component={LoginForm} title="Please Login" />
+			<Stack key="root" modal hideNavBar>
+				<Scene key="auth">
+					<Scene
+						key="login"
+						component={LoginForm}
+						title="Please Login"
+					/>
+				</Scene>
+
+				<Scene key="main">
+					<Scene
+						onRight={() => console.log("Right!!")}
+						rightTitle="Add"
+						key="employeeList"
+						component={EmployeeList}
+						title="Employees"
+					/>
+				</Scene>
 			</Stack>
 		</Router>
 	);
