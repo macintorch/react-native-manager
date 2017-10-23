7533a783df711ef35ed6500dcbaee534a87abbdc
Ainor Syahrizal <macintorch@gmail.com>
2017-10-23 15:09:16 +0800

    setup onDecline method

--- src/components/EmployeeEdit.js
+++ src/components/EmployeeEdit.js
@@ -21,6 +21,12 @@ class EmployeeEdit extends Component {
 		Communications.text(phone, `Your upcoming shift is on ${shift}`);
 	}
 
+	onAccept() {}
+
+	onDecline() {
+		this.setState({ showModal: false });
+	}
+
 	onButtonPress() {
 		const { name, phone, shift } = this.props;
 
@@ -57,7 +63,11 @@ class EmployeeEdit extends Component {
 					</Button>
 				</CardSection>
 
-				<Confirm visible={this.state.showModal}>
+				<Confirm
+					visible={this.state.showModal}
+					onAccept={this.onAccept.bind(this)}
+					onDecline={this.onDecline.binda(this)}
+				>
 					Are you sure you want to delete this?
 				</Confirm>
 			</Card>
